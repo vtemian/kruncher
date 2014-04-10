0f725a062db9d5787be77e2f5363b34ceb661a22
vtemian <vladtemian@gmail.com>
2014-04-11 02:05:44 +0300

    Simple api logic for parsing

--- analyser/api.py
+++ analyser/api.py
@@ -1,7 +1,12 @@
+import os
+import requests
+
 from flask import Blueprint
 
 from utils.decorators import validate, require
 from utils.validators import validate_url
+
+from .parser import Parser
 
 endpoint = Blueprint('analyse_url', __name__)
 
@@ -12,5 +17,16 @@ endpoint = Blueprint('analyse_url', __name__)
     'url': validate_url
 })
 def analyse_url(url):
-  print url
+  name, ext = os.path.splitext(url)
+  parse = Parser(ext=ext[1:])
+
+  response = requests.get(url, stream=True)
+  fields = []
+  for chunk in response.iter_lines(1024):
+    fields = parse(chunk)
+    if fields:
+      break
+
+  print fields
+
   return url
