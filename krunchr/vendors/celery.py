3fd81adbcf801580b42bb31367f38e9c209d16ea
vtemian <vladtemian@gmail.com>
2014-04-11 02:56:53 +0300

    Connect to rethink in celery vendor

--- krunchr/vendors/celery.py
+++ krunchr/vendors/celery.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import rethinkdb as r
 from celery import Celery
 
 from krunchr.app import get_config
@@ -8,3 +9,10 @@ celery = Celery('krunchr')
 
 config = get_config()
 celery.config_from_object(config)
+
+db = r.connect(**{
+    'host': config.RETHINKDB_HOST,
+    'port': config.RETHINKDB_PORT,
+    'auth_key': config.RETHINKDB_AUTH,
+    'db': config.RETHINKDB_DB
+})
