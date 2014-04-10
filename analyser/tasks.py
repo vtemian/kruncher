ab4333ad10713b0df25e0ff9bb46da3a0749326f
vtemian <vladtemian@gmail.com>
2014-04-11 02:56:31 +0300

    Update job state when we finish the task

--- analyser/tasks.py
+++ analyser/tasks.py
@@ -1,19 +1,23 @@
 import os
 import time
 
+import rethinkdb as r
 import requests
 
-from krunchr.vendors.celery import celery
+from krunchr.vendors.celery import celery, db
 
 
-@celery.task
-def get_file(url, path):
+@celery.task(bind=True)
+def get_file(self, url, path):
   name, ext = os.path.splitext(url)
   name = str(int(time.time()))
 
   path = "%s/%s%s" % (path, name, ext)
 
   response = requests.get(url)
-  print path
   with open(path, 'w') as f:
     f.write(response.content)
+
+  r.table('jobs').filter({
+      'task_id': self.request.id
+  }).update({'state': 'done'}).run(db)
