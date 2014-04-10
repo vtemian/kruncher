8d97dc9fc1134957d0341501992e90039f97f2a0
vtemian <vladtemian@gmail.com>
2014-04-11 01:23:50 +0300

    Return original url

--- utils/validators.py
+++ utils/validators.py
@@ -9,7 +9,5 @@ def validate_url(url):
   _url = urlparse.urlsplit(url, 'http')
   if _url.scheme != 'http':
     raise ValidationError("Invalid scheme '%s', should be http" % _url.scheme)
-  elif not url.startswith('http://'):
-    _url = urlparse.urlsplit('http://%s' % url, 'http')
 
-  return _url
+  return url
