542b98e3d3e42b5e54d59e789b8b56b694f63a0d
vtemian <vladtemian@gmail.com>
2014-04-11 21:07:44 +0300

    Added logging

--- krunchr/settings/production.py
+++ krunchr/settings/production.py
@@ -1,1 +1,55 @@
 from krunchr.settings.base import *
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {},
+    'formatters': {
+        'syslog': {
+            'format': '%(asctime)s ' + HOSTNAME_SHORT +
+                      ' %(name)s: %(levelname)s: %(message)s',
+            'datefmt': '%b %d %H:%M:%S',
+        }
+    },
+    'handlers': {
+        'logfile': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'level': 'INFO',
+            'formatter': 'syslog',
+            'when': 'midnight',
+            'backupCount': 5,
+            'filename': '/var/log/krunchr/app.log',
+        },
+        'gunicorn': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'level': 'INFO',
+            'formatter': 'syslog',
+            'when': 'midnight',
+            'backupCount': 5,
+            'filename': '/var/log/krunchr/gunicorn.log',
+        },
+        'celery': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'level': 'INFO',
+            'formatter': 'syslog',
+            'when': 'midnight',
+            'backupCount': 5,
+            'filename': '/var/log/krunchr/celery.log',
+        },
+
+
+    },
+    'loggers': {
+        'gunicorn.error': {
+            'handlers': ['gunicorn'],
+        },
+        'krunchr': {
+            'handlers': ['logfile'],
+        },
+        'celery.task': {
+            'handlers': ['celery'],
+            'level': 'INFO'
+        },
+    },
+}
--- krunchr/vendors/celery.py
+++ krunchr/vendors/celery.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import rethinkdb as r
 from celery import Celery
 
-from krunchr.app import get_config
+from krunchr.app import get_config, configure_logging
 
 celery = Celery('krunchr')
 
@@ -16,3 +16,6 @@ db = r.connect(**{
     'auth_key': config.RETHINKDB_AUTH,
     'db': config.RETHINKDB_DB
 })
+
+if hasattr(config, 'LOGGING'):
+  configure_logging(config.LOGGING)
