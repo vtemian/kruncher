7ae411293aecdacab929141a322f86a5743c841e
root <root@static.245.206.47.78.clients.your-server.de>
2014-04-12 09:02:42 +0200

    Simple api call implementation1

--- map/api.py
+++ map/api.py
@@ -1,5 +1,7 @@
+import os
 import json
 
+import rethinkdb as r
 from flask import Blueprint
 
 from utils.decorators import validate, require
@@ -8,16 +10,24 @@ from utils.validators import simple_validation, validate_uuid
 endpoint = Blueprint('maps_jobs', __name__)
 
 
-@endpoint.route('map/', methods=['POST'])
-@require('fields', 'operation', 'ds_id')
+@endpoint.route('map/group_by/', methods=['POST'])
+@require('fields', 'operation', 'ds_id', 'group_by')
 @validate({
     'fields': simple_validation,
     'operation': simple_validation,
     'ds_id': validate_uuid,
+    'group_by': lsimple_validation,
 })
-def map(fields, operation, ds_id):
-  import os
-  os.system("python map/jobs/sum.py %s 0 1 2" % ds_id)
+def map(fields, operation, ds_id, group_by):
+  dataset = r.table('datasets').filter({
+      'ds_id': ds_id
+  }).run(db.conn)
+  fields = [dataset['fields'].index(field) for field in fields]
+  group_by = dataset['fields'].index(group_by)
+
+  command = "python map/jobs/sum.py %s %s %s" % (ds_id, group_by, ' '.join(fields))
+  execute_async.delay(command)
+
   return json.dumps({
       'status': 'success',
       'message': json.dumps([])
