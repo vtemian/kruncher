e5b6053aeb19247e28f77af55cf7d085cac5535d
vtemian <vladtemian@gmail.com>
2014-04-12 10:06:24 +0300

    More pep8

--- map/api.py
+++ map/api.py
@@ -1,4 +1,3 @@
-import os
 import json
 
 import rethinkdb as r
@@ -6,6 +5,9 @@ from flask import Blueprint
 
 from utils.decorators import validate, require
 from utils.validators import simple_validation, validate_uuid
+from utils.tasks import execute_async
+
+from krunchr.vendors.rethinkdb import db
 
 endpoint = Blueprint('maps_jobs', __name__)
 
@@ -16,19 +18,20 @@ endpoint = Blueprint('maps_jobs', __name__)
     'fields': simple_validation,
     'operation': simple_validation,
     'ds_id': validate_uuid,
-    'group_by': lsimple_validation,
+    'group_by': simple_validation,
 })
 def map(fields, operation, ds_id, group_by):
   dataset = r.table('datasets').filter({
       'ds_id': ds_id
   }).run(db.conn)
+
   fields = [dataset['fields'].index(field) for field in fields]
   group_by = dataset['fields'].index(group_by)
 
-  command = "python map/jobs/sum.py %s %s %s" % (ds_id, group_by, ' '.join(fields))
+  command = "python map/jobs/sum.py %s %s %s" % (ds_id, group_by,
+                                                 ' '.join(fields))
   execute_async.delay(command)
 
   return json.dumps({
       'status': 'success',
-      'message': json.dumps([])
   })
